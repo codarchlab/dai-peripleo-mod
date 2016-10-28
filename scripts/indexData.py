79500aaa35e79c0ef4adbc880ae248f7f22dbb9e
Rainer Simon <rainer.simon@ait.ac.at>
2016-10-28 10:01:37 +0200

    Removed outdated convert/index scripts

--- scripts/indexData.py
+++
@@ -1,8 +0,0 @@
-import json
-import requests
-
-with open('../data/west-2015.wgs84.idig.json.txt') as f:
-  for line in f.readlines():
-    item = json.loads(line)
-    response = requests.post('http://localhost:8983/solr/peripleo/update?commit=true', json=[ item ])
-    print(response.text)
